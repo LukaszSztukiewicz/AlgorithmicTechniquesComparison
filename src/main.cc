54d9b75fdb893c323aa89183e98d52003612f45c
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-06-03 21:23:24 +0200

    set output files

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,10 @@
 #include "AlgorithmicTechniquesComparison.h"
 #include "util/csv.h"
+#include <fstream>
 
-#define outQualityFile1 "./data/out/quality1.csv"
-#define outQualityFile2 "./data/out/quality2.csv"
-#define outTimeFile "./data/out/time.csv"
+#define outQualityFile "./data/out/quality.csv"
+#define outTimeFile1 "./data/out/time1.csv"
+#define outTimeFile2 "./data/out/time2.csv"
 #define inInstancesFolder "./data/in/"
 
 void time_comparison_Greedy_DynamicProgramming_Exhaustive() {
@@ -11,7 +12,8 @@ void time_comparison_Greedy_DynamicProgramming_Exhaustive() {
   int stepNumberOfItems    = 1;
   int maxNumberOfItems     = 30;
 
-  std::ofstream file_results_time;
+  std::fstream file_results_time;
+  file_results_time.open(outQualityFile, std::fstream::out);
   file_results_time << "numberOfItems,greedy,dynamicProgramming,exhaustive" << std::endl;
 
   // vector of solving functions
@@ -47,7 +49,8 @@ void time_comparison_Greedy_DynamicProgramming() {
   int stepNumberOfItems    = 500;
   int maxNumberOfItems     = 10000;
 
-  std::ofstream file_results_time;
+  std::fstream file_results_time;
+  file_results_time.open(outQualityFile, std::fstream::out);
   file_results_time << "numberOfItems,greedy,dynamicProgramming" << std::endl;
 
   // vector of solving functions
@@ -83,8 +86,9 @@ void quality_comparison_Greedy_DynamicProgramming() {
   int stepNumberOfItems    = 500;
   int maxNumberOfItems     = 10000;
 
-  std::ofstream file_results_time;
-  file_results_time << "numberOfItems,greedy,dynamicProgramming" << std::endl;
+  std::fstream file_results_quality;
+  file_results_quality.open(outQualityFile, std::fstream::out);
+  file_results_quality << "numberOfItems,greedy,dynamicProgramming" << std::endl;
 
   // vector of solving functions
   std::vector<std::function<std::vector<Item>(std::vector<Item>, int)>> solvingFunctions = {
@@ -93,7 +97,7 @@ void quality_comparison_Greedy_DynamicProgramming() {
   };
 
   for (int i = initialNumberOfItems; i < maxNumberOfItems; i += stepNumberOfItems) {
-    file_results_time << i << ",";
+    file_results_quality << i << ",";
     int knapsackCapacity = floor(i * 0.6);
     int maxWeight        = floor(i * 0.4);
 
@@ -102,15 +106,16 @@ void quality_comparison_Greedy_DynamicProgramming() {
     std::vector<Item> items = AlgorithmicTechniquesComparison::generateRandomItems(i, maxWeight);
 
     for (auto solvingFunction : solvingFunctions) {
-      auto start = std::chrono::high_resolution_clock::now();
 
       auto solution = solvingFunction(items, knapsackCapacity); // key function
 
-      auto end     = std::chrono::high_resolution_clock::now();
-      auto time_ch = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); // milliseconds
-      file_results_time << time_ch << ",";
+      int solutionQuality = 0;
+      for (auto item : solution) {
+        solutionQuality += item.value;
+      }
+      file_results_quality << solutionQuality << ",";
     }
-    file_results_time << std::endl;
+    file_results_quality << std::endl;
   }
 }
 
